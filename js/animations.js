26137959167d79759d728e523d77d29f0d3d6374
atharvcapricorn07 <atharv.capricorn@gmail.com>
2025-05-26 20:07:06 -0700

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -4,27 +4,25 @@ const darkOverlay = document.getElementById('dark-overlay');
 themeToggle.addEventListener('click', () => {
   const isDark = document.body.classList.contains('dark-mode');
 
-  // Remove any existing transition classes
+  // Clean previous transition classes
   darkOverlay.classList.remove('slide-in', 'slide-out-left', 'slide-out-right');
 
-  if (isDark) {
-    // Exiting dark mode
-    darkOverlay.classList.add('slide-out-left');
-
-    setTimeout(() => {
-      document.body.classList.remove('dark-mode');
-    }, 500);
-  } else {
-    // Entering dark mode
+  if (!isDark) {
+    // Slide in to enter dark mode
     darkOverlay.classList.add('slide-in');
 
     setTimeout(() => {
       document.body.classList.add('dark-mode');
+      darkOverlay.classList.remove('slide-in');
+    }, 500);
+  } else {
+    // Slide out to right to leave dark mode
+    darkOverlay.classList.add('slide-out-right');
+
+    setTimeout(() => {
+      document.body.classList.remove('dark-mode');
+      darkOverlay.classList.remove('slide-out-right');
+      darkOverlay.classList.add('slide-out-left'); // Reset state
     }, 500);
   }
-
-  // Clean up overlay after transition
-  setTimeout(() => {
-    darkOverlay.classList.remove('slide-in', 'slide-out-left', 'slide-out-right');
-  }, 1000);
 });
